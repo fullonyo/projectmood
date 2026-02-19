844d0b6b05e660e2629be21dee994c62b3641a23
Maikonj-hash <maikon.julio@trackland.com.br>
2026-02-19 00:50:33 -0400

    feat: infrastructure and rebranding to MoodSpace

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -2,20 +2,20 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 export interface InputProps
-    extends React.InputHTMLAttributes<HTMLInputElement> { }
+  extends React.InputHTMLAttributes<HTMLInputElement> { }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-    ({ className, type, ...props }, ref) => {
-        return (
-            <input
-        type= { type }
-        className = {
-            cn(
-          "flex h-10 w-full rounded-xl border border-zinc-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-zinc-800 dark:bg-zinc-950 dark:ring-offset-zinc-950 dark:placeholder:text-zinc-400 dark:focus-visible:ring-zinc-300",
-                className
-            )
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={
+          cn(
+            "flex h-10 w-full rounded-xl border border-zinc-200 bg-white px-3 py-2 text-sm text-zinc-950 ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-zinc-800 dark:bg-zinc-950 dark:text-zinc-50 dark:ring-offset-zinc-950 dark:placeholder:text-zinc-400 dark:focus-visible:ring-zinc-300",
+            className
+          )
         }
-        ref = { ref }
+        ref={ref}
         {...props}
       />
     )
